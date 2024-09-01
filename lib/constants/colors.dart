873623587562be4b2d554301ee4c1e71322e4791
Buddhima Fernando <harun.fernando.hf@gmail.com>
2024-09-01 09:40:06 +0530

    chore: Update app colors in colors.dart

---
+++ lib/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // app colors
+  Color primaryBGColor = const Color.fromARGB(255, 255, 255, 255);
+  Color primaryFeildColor = const Color.fromARGB(255, 235, 235, 235);
+  // button color
+  Color primaryColor = const Color.fromARGB(255, 27, 61, 89);
+  // text colors
+  Color contentColor = const Color.fromARGB(255, 80, 80, 80);
+  Color primaryTextColor = const Color.fromARGB(255, 68, 68, 68);
+  // text fields and tiles
+  Color fieldTextColor = const Color.fromARGB(47, 103, 103, 103);
+  Color tileColor = const Color.fromARGB(255, 243, 243, 243);
+}
