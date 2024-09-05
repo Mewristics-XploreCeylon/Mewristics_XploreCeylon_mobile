69a763f5638275411f0cf42ce3e85a912aae125a
semage-02 <bhagya.semage@gmail.com>
2024-09-05 22:47:27 +0530

    Updated route folder

--- lib/config/app_router.dart/routes.dart
+++ lib/config/app_router.dart/routes.dart
@@ -1,11 +1,15 @@
+
+
 class Routes {
   static const splash = '/';
 
   static const visaOnboarding = 'visaOnboarding';
   static const biometricAuthentication = 'biometricAuthentication';
   static const visaPersonalInformation = 'visaPersonalInformation';
+  static const applicantsDetails = 'applicantsDetails';
   static const presentPassportDetails = 'presentPassportDetails';
   static const previousPassportDetails = 'previousPassportDetails';
+   static const familyBackground = 'familyBackground';
   static const emergencyContacts = 'emergencyContacts';
   static const visitDetails = 'visitDetails';
   static const visaApplicationFee = 'visaApplicationFee';
